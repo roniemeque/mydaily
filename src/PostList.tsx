98b4f2352a71c10d4a3b2c44204f1dca6e4eebb4
Ronie <roniemeque@icloud.com>
2020-05-24 02:42:27 -0300

    caching locally

--- src/PostList.tsx
+++ src/PostList.tsx
@@ -1,5 +1,8 @@
 import React, { FC, useEffect, useState } from "react";
 import { fetchApi } from "./helpers/graphql";
+import { getItemStorage, setItemStorage } from "./helpers/localstorage";
+
+const MINUTES_TO_EXPIRE = 5;
 
 interface Post {
   id: string;
@@ -40,10 +43,21 @@ const fetchPosts = async (): Promise<Post[]> => {
 };
 
 const PostList: FC = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
+  const [posts, setPosts] = useState<Post[]>(
+    getItemStorage("posts", 1000 * 60 * MINUTES_TO_EXPIRE) ?? []
+  );
 
   useEffect(() => {
-    fetchPosts().then((data) => setPosts(data));
+    if (!posts.length) {
+      fetchPosts().then((data) => {
+        console.log("fetching new posts");
+
+        setPosts(data);
+        setItemStorage("posts", data);
+      });
+    } else {
+      console.log("got from localstorage");
+    }
   }, []);
 
   return (
