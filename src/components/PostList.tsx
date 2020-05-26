7950bc65100a299c9a06d374ba8dcc0f071552ab
Ronie <roniemeque@icloud.com>
2020-05-25 23:40:41 -0300

    moving posts fetching into hook

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,60 +1,10 @@
-import React, { FC, useEffect, useState } from "react";
-import { fetchApi } from "../helpers/graphql";
-import { getItemStorage, setItemStorage } from "../helpers/localstorage";
 import styled from "@emotion/styled";
+import React, { FC } from "react";
+import usePosts from "../hooks/usePosts";
 import PostCard from "./PostCard";
 
-const MINUTES_TO_EXPIRE = 5;
-
-const latestTiming = new Date().toISOString();
-
-const POSTS_QUERY = `
-  query Latest($params: QueryPostInput) {
-    latest(params: $params) {
-      id
-      url
-      title
-      image
-      # readTime
-      tags
-      publication {
-        id
-        name
-      }
-    }
-  }
-`;
-
-const fetchPosts = async (): Promise<Post[]> => {
-  const { latest } = await fetchApi(POSTS_QUERY, {
-    params: {
-      latest: latestTiming,
-      page: 0,
-      pageSize: 20,
-      sortBy: "popularity",
-    },
-  });
-
-  return latest;
-};
-
 const PostList: FC = () => {
-  const [posts, setPosts] = useState<Post[]>(
-    getItemStorage("posts", 1000 * 60 * MINUTES_TO_EXPIRE) ?? [],
-  );
-
-  useEffect(() => {
-    if (!posts.length) {
-      fetchPosts().then((data) => {
-        console.log("fetching new posts");
-
-        setPosts(data);
-        setItemStorage("posts", data);
-      });
-    } else {
-      console.log("got from localstorage");
-    }
-  }, []);
+  const [posts] = usePosts();
 
   return (
     <List>
